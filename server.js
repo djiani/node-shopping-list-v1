b8eee5c24d631461d34b93e7bcf0fdc3b49cb64b
Samuel R. Djiani <djiasara87@yahoo.fr>
2017-10-03 21:31:20 -0400

    implement post method for both shopping list and recipes list

--- server.js
+++ server.js
@@ -18,15 +18,53 @@ ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
 
+// adding some recipes to `Recipes` so there's something
+// to retrieve.
+Recipes.create(
+  'boiled white rice', ['1 cup white rice', '2 cups water', 'pinch of salt']);
+Recipes.create(
+  'milkshake', ['2 tbsp cocoa', '2 cups vanilla ice cream', '1 cup milk']);
+
 // when the root of this router is called with GET, return
 // all current ShoppingList items
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
 });
 
-//adding some recipes
-Recipes.create('chocolate milk', ['cocoa', 'milk', 'sugar']);
-//create the endpoints
+app.post('/shopping-list',jsonParser, (req, res)=>{
+  //ensure name and budget are in the request body
+  const requiredField = ['name', 'budget'];
+  for(let i= 0; i < requiredField.length; i++){
+    const field = requiredField[i];
+    if (!(field in req.body)){
+      const message = `missing \'${field}\' in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+    const item = ShoppingList.create(req.body.name, req.body.budget);
+    res.status(201).json(item);
+
+  }
+});
+
+app.post('/recipes', jsonParser, (req, res)=>{
+  const requiredField = ['name', 'ingredients'];
+  for(let i= 0; i < requiredField.length; i++){
+    const field = requiredField[i];
+    if (!(field in req.body)){
+      const message = `missing \'${field}\' in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+    const item = Recipes.create(req.body.name, req.body.ingredients);
+    res.status(201).json(item);
+
+  }
+});
+
+
+//when the root of this router is called with GET, return
+// all current Recipes list
 app.get('/recipes', (req, res) =>{
   res.json(Recipes.get());
 });
