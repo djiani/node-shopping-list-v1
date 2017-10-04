d73a1ee919840fcf4b38ad4550967a7051b613d3
Samuel R. Djiani <djiasara87@yahoo.fr>
2017-10-04 16:11:48 -0400

    modular code

--- server.js
+++ server.js
@@ -4,137 +4,30 @@ const router = express.Router();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-const {ShoppingList, Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
 
+const shoppingListRouter = require('./shoppingListRouter');
+const recipesRouter = require('./recipesRouter');
+
 // log the http layer
 app.use(morgan('common'));
 
-// we're going to add some items to ShoppingList
-// so there's some data to look at
-ShoppingList.create('beans', 2);
-ShoppingList.create('tomatoes', 3);
-ShoppingList.create('peppers', 4);
+app.use(express.static('public'))
 
-// adding some recipes to `Recipes` so there's something
-// to retrieve.
-Recipes.create(
-  'boiled white rice', ['1 cup white rice', '2 cups water', 'pinch of salt']);
-Recipes.create(
-  'milkshake', ['2 tbsp cocoa', '2 cups vanilla ice cream', '1 cup milk']);
-
-// when the root of this router is called with GET, return
-// all current ShoppingList items
-app.get('/shopping-list', (req, res) => {
-  res.json(ShoppingList.get());
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 
-app.post('/shopping-list',jsonParser, (req, res)=>{
-  //ensure name and budget are in the request body
-  const requiredField = ['name', 'budget'];
-  for(let i= 0; i < requiredField.length; i++){
-    const field = requiredField[i];
-    if (!(field in req.body)){
-      const message = `missing \'${field}\' in request body`;
-      console.error(message);
-      return res.status(400).send(message);
-    }
-    const item = ShoppingList.create(req.body.name, req.body.budget);
-    res.status(201).json(item);
+// when requests come into `/shopping-list` or
+// `/recipes`, we'll route them to the express
+// router instances we've imported. Remember,
+// these router instances act as modular, mini-express apps.
+app.use('/shopping-list', shoppingListRouter);
+app.use('/recipes', recipesRouter);
 
-  }
-});
-
-
-app.delete('/shopping-list/:id', (req, res)=>{
-  ShoppingList.delete(req.params.id);
-  console.log(`Delete shopping list item ${req.params.id}`);
-  res.status(204).end();
-})
-
-app.put('/shopping-list/:id', jsonParser, (req, res)=>{
-  const requiredFields = ['name', 'budget', 'id'];
-  for(let i =0; i < requiredFields.length; i++){
-    const field = requiredFields[i];
-    if(!(field in req.body)){
-      const message = `Missing  ${field} in request body`;
-      console.error(message);
-      return res.status(400).send(message);
-    }
-  }
-  //how do you get the params here: req.params.id????
-  if(req.params.id != req.body.id){    
-    console.log('here');
-    const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
-    console.error(message);
-    return res.status(400).send(message);
-  }
-  console.log(`Updating shopping list item \`${req.params.id}\``);
-  ShoppingList.update({
-    id: req.params.id,
-    name: req.body.name,
-    budget: req.body.budget
-  });
-  res.status(204).end();
-
-})
-
-//when the root of this router is called with GET, return
-// all current Recipes list
-app.get('/recipes', (req, res) =>{
-  res.json(Recipes.get());
-});
-
-app.post('/recipes', jsonParser, (req, res)=>{
-  const requiredField = ['name', 'ingredients'];
-  for(let i= 0; i < requiredField.length; i++){
-    const field = requiredField[i];
-    if (!(field in req.body)){
-      const message = `missing \'${field}\' in request body`;
-      console.error(message);
-      return res.status(400).send(message);
-    }
-    const item = Recipes.create(req.body.name, req.body.ingredients);
-    res.status(201).json(item);
-
-  }
-});
-
-app.delete('/recipes/:id', (req, res)=>{
-  Recipes.delete(req.params.id);
-  console.log(`Delete shopping list item ${req.params.id}`);
-  res.status(204).end();
-})
-
-app.put('/recipes/:id', jsonParser, (req, res)=>{
-  const requiredFields = ['name', 'ingredients', 'id'];
-  for(let i =0; i < requiredFields.length; i++){
-    const field = requiredFields[i];
-    if(!(field in req.body)){
-      const message = `Missing  ${field} in request body`;
-      console.error(message);
-      return res.status(400).send(message);
-    }
-  }
-  //how do you get the params here: req.params.id????
-  if(req.params.id != req.body.id){    
-    console.log('here');
-    const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
-    console.error(message);
-    return res.status(400).send(message);
-  }
-  console.log(`Updating shopping list item \`${req.params.id}\``);
-  Recipes.update({
-    id: req.params.id,
-    name: req.body.name,
-    ingredients: req.body.ingredients
-  });
-  res.status(204).end();
-
-})
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
